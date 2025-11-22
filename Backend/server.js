f732d3d2e3c1131f8ca9d54489ca1b30aec83ccf
Oliver Iraheta <oliverjared57@gmail.com>
2025-11-22 13:44:51 -0600

    CDP connection

--- Backend/server.js
+++ Backend/server.js
@@ -5,9 +5,9 @@ const telegramService = require("./src/services/telegram.service");
 const app = express();
 const userRoutes = require("./src/routes/user.routes");
 const telegramRoutes = require("./src/routes/telegram.routes");
+const { CdpClient } = require("@coinbase/cdp-sdk");
 
-// Initialize Telegram Bot
-telegramService.initBot();
+const cdp = new CdpClient();
 
 // Middlewares
 app.use(express.json());
@@ -16,8 +16,28 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/telegram", telegramRoutes);
 
-// Inicializar servidor
-const PORT = config.port;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+// Inicializar Telegram Bot
+telegramService.initBot();
+
+async function init() {
+  //Ejemplo de como Crear cuenta EVM
+  const account = await cdp.evm.createAccount();
+  console.log(`Created EVM account: ${account.address}`);
+
+  const faucetResponse = await cdp.evm.requestFaucet({
+    address: account.address,
+    network: "ethereum-sepolia",
+    token: "eth",
+  });
+  console.log(
+    `Requested funds from ETH faucet: https://sepolia.basescan.org/tx/${faucetResponse.transactionHash}`
+  );
+
+  // Inicializar servidor
+  const PORT = config.port;
+  app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  });
+}
+
+init();
