6d36789be8357cf905a3fcc7ff1022afcfd923c1
cooldev1337 <cooldev1337@hotmail.com>
2025-11-23 06:30:26 -0300

    uploadTxtToStorage deals with string shorter than the minimum size

--- Backend/src/utils/filecoin.js
+++ Backend/src/utils/filecoin.js
@@ -1,16 +1,16 @@
 import { Synapse, RPC_URLS } from "@filoz/synapse-sdk";
 
-export async function uploadTxtToStorage(data) {
+export async function uploadTxtToStorage(text) {
   try {
     const synapse = await Synapse.create({
       privateKey: process.env.FileCoinKey,
       rpcURL: RPC_URLS.calibration.http,
     });
 
-    const encodedDAta = new TextEncoder().encode(data);
+    const encodedData = new TextEncoder().encode(text.length >= 127 ? text : text.padEnd(127, ' '));
     // const { pieceCid, size } 
-    console.log("before synapse upload", encodedDAta)
-    const uploadResult = await synapse.storage.upload(encodedDAta)
+    console.log("before synapse upload", encodedData)
+    const uploadResult = await synapse.storage.upload(encodedData)
     console.log(`PieceCID: ${uploadResult.pieceCid}`);
     console.log(`Size: ${uploadResult.size} bytes`);
 
